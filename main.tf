b17d8dffbb2d766e57745584895cc7a8af71a48f
yuta051214 <ffftrick123@gmail.com>
2023-05-30 02:21:15 +0900

    fix :terraform fmt

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "aws-ecs-terraform-tfstate-test"
-    key = "terraform.tfstate"
-    region = "ap-northeast-1"
+    bucket         = "aws-ecs-terraform-tfstate-test"
+    key            = "terraform.tfstate"
+    region         = "ap-northeast-1"
     dynamodb_table = "aws-ecs-terraform-tfstate-locking"
-    encrypt = true
+    encrypt        = true
   }
 }
