75b17b4394f2f42983a8e2346ffc6d9b34ee8cca
yuta051214 <ffftrick123@gmail.com>
2023-06-01 14:23:37 +0900

    delete resources

--- cloudwatch.tf
+++
@@ -1,5 +0,0 @@
-# コンテナのログ出力先として CloudWatch Logs にロググループを作成
-resource "aws_cloudwatch_log_group" "cloudwatch_log_group" {
-  name              = "/aws/ecs/${local.app}"
-  retention_in_days = 1 # ログの保存期間
-}
--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# main.tf では、Terraform のバックエンドの設定を行なっています。ここで指定された S3 バケット内にある tfstate ファイルと DynamoDB を参照してインフラストラクチャーを管理しています。
+# main.tf のコードを削除すると、現在のアーキテクチャの状態を取得できなくなり、正常にリソースを削除することができません。
+
 terraform {
   backend "s3" {
     bucket         = "aws-ecs-terraform-tfstate-test"
